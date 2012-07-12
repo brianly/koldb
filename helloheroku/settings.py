99f8b42b8d47b4aa71f42c71f17bfa3a5019663f
Brian Lyttle <brian+github@killaloo.org>
2012-07-12 14:40:20 -0400

    Adding another template directory

--- helloheroku/settings.py
+++ helloheroku/settings.py
@@ -123,7 +123,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/brianly/dev/github/koldb/templates/'
+    '/Users/brianly/dev/github/koldb/templates/', '/app/templates/'
 )
 
 INSTALLED_APPS = (
