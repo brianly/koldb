287b93be2873de2264c534694ae3f6c52af0de8d
Brian Lyttle <brian+github@killaloo.org>
2012-07-20 00:28:40 -0400

    Revert to heroku DB settings

--- helloheroku/settings.py
+++ helloheroku/settings.py
@@ -6,15 +6,15 @@ import dj_database_url
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DATABASES = {
-    # 'default': dj_database_url.config(default='postgres://localhost'),
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'testdb.sqlite3',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default='postgres://localhost'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    #     'NAME': 'testdb.sqlite3',                      # Or path to database file if using sqlite3.
+    #     'USER': '',                      # Not used with sqlite3.
+    #     'PASSWORD': '',                  # Not used with sqlite3.
+    #     'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+    #     'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    # }
 }
 
 FIXTURE_DIRS = (os.path.join(PROJECT_ROOT, 'fixtures'),)
